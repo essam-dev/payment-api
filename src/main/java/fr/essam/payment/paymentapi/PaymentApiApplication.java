ece738438b3a55504dc0240d6da8b3fc76b225aa
Essam <essam.elmanser@gmail.com>
2024-09-23 17:29:26 +0200

    init repo

---
+++ src/main/java/fr/essam/payment/paymentapi/PaymentApiApplication.java
@@ -0,0 +1,13 @@
+package fr.essam.payment.paymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/essam/payment/paymentapi/PaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.essam.payment.paymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
